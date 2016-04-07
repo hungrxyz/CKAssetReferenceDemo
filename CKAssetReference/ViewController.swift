6dda85efead623eaf8dcee89a36994ddec34eed6
zzzel <marko@zzzel.xyz>
2016-04-07 02:51:24 +0200

    Initial Commit

---
+++ CKAssetReference/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CKAssetReference
+//
+//  Created by Zel Marko on 4/7/16.
+//  Copyright © 2016 Zel Marko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
