fb6d28b6981809af333e8494caa76a0176a69cd3
zzzel <marko@zzzel.xyz>
2016-04-07 03:39:00 +0200

    Initial Commit

--- CKAssetReference/ViewController.swift
+++ CKAssetReference/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import CloudKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		let imagePickerController = UIImagePickerController()
+		imagePickerController.delegate = self
+		imagePickerController.sourceType = .PhotoLibrary
+		
+		presentViewController(imagePickerController, animated: true, completion: nil)
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+		
+		let localPath = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent(NSUUID().UUIDString)
+		
+		let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+		let data = UIImageJPEGRepresentation(image, 0.1)
+		data!.writeToURL(localPath, atomically: true)
+		
+		let asset = CKAsset(fileURL: localPath)
+		let record = CKRecord(recordType: "Image")
+		record["image"] = asset
+		record["user"] = CKReference(recordID: CKRecordID(recordName: "UserID"), action: .DeleteSelf)
+		// You can also use the CKReference(record: *userRecord*, action .DeleteSelf) if you alredy have the record
+		
+		CKContainer.defaultContainer().publicCloudDatabase.saveRecord(record) { (savedRecord, err) in
+			if let err = err {
+				print(err)
+			} else {
+				print(savedRecord)
+			}
+		}
+		
 	}
-
-
 }
 
